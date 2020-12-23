9e6598a8c71220a49ae38583d066d0f7fe10a32e
Sean Young <seanyoung247@gmail.com>
2020-12-22 21:46:51 -0500

    Chore: Initial python code
    Adds basic app.py and enviroment variables

---
+++ app.py
@@ -0,0 +1,29 @@
+import os
+from flask import ( Flask, flash, render_template, redirect,
+                    request, session, url_for, abort)
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
+from werkzeug.security import generate_password_hash, check_password_hash
+if os.path.exists("env.py"):
+    import env
+
+DEBUGGING = True
+
+app = Flask(__name__)
+
+app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
+app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
+app.secret_key = os.environ.get("SECRET_KEY")
+
+mongo = PyMongo(app)
+
+
+@app.route("/")
+def test():
+    return "plum lives!"
+
+
+if __name__ == "__main__":
+    app.run(host=os.environ.get("IP"),
+            port=int(os.environ.get("PORT")),
+            debug=DEBUGGING)
