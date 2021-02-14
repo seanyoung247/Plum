d4b08015626a9b4f11294104c05624c80be7b47b
Sean Young <seanyoung247@gmail.com>
2021-02-14 22:33:26 +0000

    Feat - Initial login backend code

--- app.py
+++ app.py
@@ -36,6 +36,24 @@ def home():
 #login
 @app.route("/login", methods=["GET", "POST"])
 def login():
+    if request.method == "POST":
+        #check if username exists in db
+        existing_user = mongo.db.users.find_one(
+            {"name": request.form.get("username").lower()})
+
+        if existing_user:
+            #ensure password matches
+            if check_password_hash(existing_user["password"], request.form.get("password")):
+                session["user"] = request.form.get("username").lower()
+                # --- flash message here --- #
+                return redirect(url_for("home"))
+            else:
+                # --- flash message here --- #
+                return redirect(url_for("login"))
+        else:
+            # --- flash message here --- #
+            return redirect(url_for("login"))
+
     return render_template("login.html")
 
 
