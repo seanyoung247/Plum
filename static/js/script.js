a81f90d3810eeb4d0561c93192ed7a3f095b47bc
Sean Young <seanyoung247@gmail.com>
2021-03-03 16:36:08 +0000

    Feat: Improves scroll component behaviour

--- static/js/script.js
+++ static/js/script.js
@@ -175,9 +175,10 @@ $( ".scroller .scroll-left" ).click(function(event) {
   if (scroller.scrollLeft() > 0) {
     // Which item is left most?
     let leftMostItem = Math.ceil((scroller.scrollLeft()) / scrollItem.outerWidth());
-    // How many items comprise a page?
+    // How many items fit in a page?
     let pageItemWidth = Math.floor(scroller.width() / scrollItem.outerWidth());
-    // Advance to new scrollPosition
+    // Calculate new scroll position: Find out which item should be left most
+    //  then calulate it's position by multiplying it by icon width
     scrollPosition = ((leftMostItem - pageItemWidth) * scrollItem.outerWidth());
   }
   scroller.animate({scrollLeft: scrollPosition}, 500);
@@ -186,17 +187,18 @@ $( ".scroller .scroll-left" ).click(function(event) {
 // Scrolls one "page" right (forwards)
 $( ".scroller .scroll-right" ).click(function(event) {
   let scroller = $( this ).siblings( ".scroller-items" );
-  let scrollItem = scroller.children( ".scroll-item" );
-  let scrollEndItem = scroller.children( ".scroll-item-bookend" );
+  let scrollItemWidth = scroller.children( ".scroll-item" ).outerWidth();
+  let scrollEndPosition = scroller.scrollLeft() + scroller.width() + scroller.children( ".scroll-item-bookend" ).outerWidth();
   let scrollPosition = 0;
   // If we're not at the end of the items move to the next page
-  if ((scroller.scrollLeft() + scroller.width() + scrollEndItem.outerWidth()) < scroller.get(0).scrollWidth) {
+  if (scrollEndPosition < (scroller.get(0).scrollWidth - 1)) {
     // Which item is left most?
-    let leftMostItem = Math.floor((scroller.scrollLeft()) / scrollItem.outerWidth());
-    // How many items comprise a page?
-    let pageItemWidth = Math.floor(scroller.width() / scrollItem.outerWidth());
-    // Advance to new scrollPosition
-    scrollPosition = ((leftMostItem + pageItemWidth) * scrollItem.outerWidth());
+    let leftMostItem = Math.floor((scroller.scrollLeft()) / scrollItemWidth);
+    // How many items fit in a page?
+    let pageItemWidth = Math.floor(scroller.width() / scrollItemWidth);
+    // Calculate new scroll position: Find out which item should be left most
+    //  then calulate it's position by multiplying it by icon width
+    scrollPosition = ((leftMostItem + pageItemWidth) * scrollItemWidth);
   }
   scroller.animate({scrollLeft: scrollPosition}, 500);
 });
