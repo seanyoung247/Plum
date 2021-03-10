42201f3862bcd193ec204902f4dfa95dfb4a6fa8
Sean Young <seanyoung247@gmail.com>
2021-03-10 21:31:50 +0000

    Feat: Ensures rating search isn't done on 0 rating

--- app.py
+++ app.py
@@ -69,7 +69,7 @@ def search():
                     "value" : request.form["time"]
                 })
 
-        if "rating" in request.form and request.form["rating"]:
+        if "rating" in request.form and int(request.form["rating"]) > 0:
             rating = int(request.form["rating"])
             query["rating"] = { "$gte" : rating }
             form_query.append({
@@ -86,6 +86,8 @@ def search():
                 "key" : "search-text",
                 "value" : request.form["search-text"]
             })
+
+        print(query)
 
         #Only search if at least one field has been passed.
         if query:
