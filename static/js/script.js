f14dfb828bf31d8e58dbca380f9bf6055322d1db
Sean Young <seanyoung247@gmail.com>
2021-03-11 22:31:26 +0000

    Feat: Separates login page scripts to a separate file

--- static/js/script.js
+++ static/js/script.js
@@ -14,58 +14,10 @@ $(document).ready(function(){
     direction: 'bottom',
     hoverEnabled: false
   });
-  $('.collapsible').collapsible();
   $('select').formSelect();
   $('.tabs').tabs();
 });
 
-/*
- * Login/registration page
- */
-// Sends an AJAX request to the server to check if username is available
-$( "#register-username" ).focusout(function(event) {
-  event.preventDefault();
-  // Make AJAX request
-  $.ajax({
-    type : "POST",
-    url : $(event.target).attr("data-validate"),
-    contentType : 'application/json;charset=UTF-8',
-    data : JSON.stringify({ "username" : $( this ).val() }),
-    success : callBackUsername
-  });
-  return false;
-});
-
-function callBackUsername(response) {
-  if ( response["exists"] ) {
-    $( "#register-username" ).removeClass("valid").addClass("invalid");
-    // Don't let the user submit the form if the username is taken
-    $( "#register-username" )[0].setCustomValidity("Username already exists");
-  } else {
-    $( "#register-username" ).removeClass("invalid").addClass("valid");
-    $( "#register-username" )[0].setCustomValidity("");
-  }
-}
-
-// Checks if confirm password field matches password field and sets it valid/invalid
-$( "#register-password" ).focusout(function(event) {
-  if ($( this ).val() === $( "#register-password-confirm" ).val()) {
-    $( "#register-password-confirm" ).removeClass("invalid").addClass("valid");
-    $( "#register-password-confirm" )[0].setCustomValidity("");
-  } else {
-    $( "#register-password-confirm" ).removeClass("valid").addClass("invalid");
-    $( "#register-password-confirm" )[0].setCustomValidity("Passwords don't match");
-  }
-});
-$( "#register-password-confirm" ).keyup(function() {
-  if ($( this ).val() === $( "#register-password" ).val()) {
-    $( this ).removeClass("invalid").addClass("valid");
-    this.setCustomValidity("");
-  } else {
-    $( this ).removeClass("valid").addClass("invalid");
-    this.setCustomValidity("Passwords don't match");
-  }
-});
 /*
  * Search Page
  */
